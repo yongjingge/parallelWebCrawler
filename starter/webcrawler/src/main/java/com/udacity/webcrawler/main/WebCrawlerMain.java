c6d656e413456a93d88d6d967a9dba6f8316d744
yongjing <near711@outlook.com>
2021-08-24 17:46:00 +0800

    Legacy crawler updated

--- starter/webcrawler/src/main/java/com/udacity/webcrawler/main/WebCrawlerMain.java
+++ starter/webcrawler/src/main/java/com/udacity/webcrawler/main/WebCrawlerMain.java
@@ -13,7 +13,6 @@ import com.udacity.webcrawler.profiler.ProfilerModule;
 import javax.inject.Inject;
 import java.io.BufferedWriter;
 import java.io.OutputStreamWriter;
-import java.io.Writer;
 import java.nio.file.Path;
 import java.util.Objects;
 
@@ -36,8 +35,32 @@ public final class WebCrawlerMain {
 
     CrawlResult result = crawler.crawl(config.getStartPages());
     CrawlResultWriter resultWriter = new CrawlResultWriter(result);
-    // TODO: Write the crawl results to a JSON file (or System.out if the file name is empty)
-    // TODO: Write the profile data to a text file (or System.out if the file name is empty)
+
+    /**
+     * Write the crawl results to a JSON file (or System.out if the file name is empty)
+     * check the value of config.getResultPath()
+     * if is non-empty, create a Path as tile name and pass path as the argument
+     * if is empty, printout result in standard output
+     */
+    if (!config.getResultPath().isEmpty()) {
+      System.out.println("Writing result into the given path ...");
+      Path path = Path.of(config.getResultPath());
+      resultWriter.write(path);
+    } else {
+      System.out.println("Printing result to standard output ...");
+      BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(System.out));
+      resultWriter.write(bufferedWriter);
+    }
+
+    /**
+     * Write the profile data to a text file (or System.out if the file name is empty)
+     * CrawlerConfiguration#profileOutputPath
+     */
+    if (!config.getProfileOutputPath().isEmpty()) {
+      profiler.writeData(Path.of(config.getProfileOutputPath()));
+    } else {
+      profiler.writeData(new OutputStreamWriter(System.out));
+    }
   }
 
   public static void main(String[] args) throws Exception {
